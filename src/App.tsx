ceae8163f63cf1a24232a5c904f151bb998b62ee
podearroz <58558680+podearroz@users.noreply.github.com>
2021-02-22 13:36:28 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ExperienceBar } from './componentes/ExperiencaBar';
+import './styles/golbal.css';
+
+
+function App() {
+  return (
+    <div className="container">
+
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
